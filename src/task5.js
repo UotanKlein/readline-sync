02416ea8960316363146816988a5814263cdaea8
UotanKlein <francferdinant2@gmail.com>
2023-10-19 22:59:38 +0300

    Change task5.js

--- src/task5.js
+++ src/task5.js
@@ -10,12 +10,12 @@ const four = readlineSync.question('y2: ');
 
 const calculateDistance = (x1, y1, x2, y2) => {
 
-    const x = Math.abs(x1) + Math.abs(x2);
-    const y = Math.abs(y1) + Math.abs(y2);
+    const x = x2 - x1;
+    const y = y2 - y1;
 
     result = Math.sqrt(x ** 2 + y ** 2);
 
-    return result;
+    return Math.abs(result);
 };
 
 console.log(`Ответ: ${calculateDistance(one, three, two, four)}`);
